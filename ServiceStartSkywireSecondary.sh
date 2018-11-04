83113c33d8e40c8f2b1b0bf14cca655473938cc5
Eli Willaert <eli.willaert@gmail.com>
2018-11-04 21:30:10 +0100

    Updated for skypi

--- ServiceStartSkywireSecondary.sh
+++ ServiceStartSkywireSecondary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $GOPATH/bin
-nohup ./node -connect-manager -manager-address 192.168.178.101:5998 -manager-web manager_node_ip:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &
+nohup ./node -connect-manager -manager-address 192.168.178.101:5998 -manager-web 192.168.178.101:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &
