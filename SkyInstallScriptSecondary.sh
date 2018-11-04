fbb067c81c005954de2c3694ea4f5bc741f30d9e
Eli Willaert <eli.willaert@gmail.com>
2018-11-04 21:41:25 +0100

    Updated for skypi

--- SkyInstallScriptSecondary.sh
+++ SkyInstallScriptSecondary.sh
@@ -109,6 +109,9 @@ echo "Skywire succesfully installed!"
 ###### Change into installation path
 ###### Starts Skywire Monitor service for the first time / after that command web interface of Monitor works
 ###### This line is to start the monitor on the slave nodes (Raspberry PI 2-8) only / Master Orange PIs requiere a different line!!!
+echo "Killing any running instances"
+ps aux | grep manager | grep -v grep | awk '{ print $2 }' | xargs kill -9
+
 cd $GOPATH/bin
 nohup ./node -connect-manager -manager-address 192.168.178.101:5998 -manager-web 192.168.178.101:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &
 echo "Skywire node is now started and you can see it in on the Monitor."
